f09e0cc73f108c7dd09cb7e5e181db47cf119e40
Subhraj <subhrajankit@gmail.com>
2026-01-26 18:36:49 +0530

    Blog page done

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -3,11 +3,16 @@ import { useParams } from 'react-router-dom';
 import { assets, blog_data, comments_data } from '../assets/assets';
 import Navbar from '../components/Navbar';
 import Moment from 'moment';
+import Footer from '../components/Footer';
+import Loader from '../components/Loader';
 
 const Blog = () => {
     const {id} = useParams();
     const [data, setData] = useState(null);
     const [comments,setComments] = useState([]);
+
+    const [name,setName] = useState('');
+    const [content,setContent] = useState('');
 
 
     const fetchBlogData = async () => {
@@ -18,6 +23,11 @@ const Blog = () => {
     const fetchComments = async () => {
         setComments(comments_data)
     }
+
+    const addComment = async (e) => {
+        e.preventDefault();
+    }
+
     useEffect(() => {
         fetchBlogData()
         fetchComments()
@@ -47,26 +57,53 @@ const Blog = () => {
            </div>
            {/* {Comments Section} */}
            <div className='mt-14 mb-10 max-w-3xl mx-auto'>
-            <p className='font-semibold mb-4'>Comments ({comments.length})</p>
-            <div className='flex flex-col gap-4'>
-                {comments.map((item, index) => (
-                    <div key={index} className='relative bg-primary/2 border border-primary/5 max-w-xl
-                    p-4 rounded text-gray-600'>
-                        <div className='flex items-center gap-2 mb-2'>
-                            <img src={assets.user_icon} alt="" className='w-6'/>
-                            <p className='font-medium'>{item.name}</p>
+                <p className='font-semibold mb-4'>Comments ({comments.length})</p>
+                <div className='flex flex-col gap-4'>
+                    {comments.map((item, index) => (
+                        <div key={index} className='relative bg-primary/2 border border-primary/5 max-w-xl
+                        p-4 rounded text-gray-600'>
+                            <div className='flex items-center gap-2 mb-2'>
+                                <img src={assets.user_icon} alt="" className='w-6'/>
+                                <p className='font-medium'>{item.name}</p>
+                            </div>
+                            <p className='text-sm max-w-md ml-8'>{item.content}</p>
+                            <div className='absolute right-4 bottom-3 flex items-center gap-2
+                            text-xs'>{Moment(item.createdAt).fromNow()}</div>
                         </div>
-                        <p className='text-sm max-w-md ml-8'>{item.content}</p>
-                        <div className='absolute right-4 bottom-3 flex items-center gap-2
-                        text-xs'>{Moment(item.createdAt).fromNow()}</div>
-                    </div>
-                ))}
+                    ))}
+                </div>                 
             </div>
 
-           </div>
+            {/* Add Comment Section */}
+
+            <div className='max-w-3xl mx-auto'>
+                    <p className='font-semibold mb-4'>Add Your Comment</p>
+                    <form onSubmit={addComment} className='flex flex-col items-start gap-4 max-w-lg'>
+                        <input onChange={(e) => setName(e.target.value)} value={name} type='text' placeholder= 'Name' required className='w-full p-2 border border-gray-300 rounded outline-none'/>
+                        <textarea onChange={(e) => setContent(e.target.value)} value={content} placeholder='Your Comment' required className='w-full p-2 border border-gray-300 rounded outline-none h-48'></textarea>
+                        <button type='submit' className= 'bg-primary text-white rounded p-2 px-8 hover:scale:102 transition-all cursor-pointer'>Submit </button>
+                    </form>
+
+            </div>
+            
+            {/* Share Buttons */}
+
+            <div className='my-24 max-w-3xl mx-auto'>
+                <p className='font-semibold my-4'>Share this article on social media</p>
+                
+                <div className = 'flex'>
+                    <img src={assets.facebook_icon} alt="" width={50}/>
+                    <img src={assets.twitter_icon} alt="" width={50}/>
+                    <img src={assets.linkedin_icon} alt="" width={50}/>
+
+                </div>
+            
+            </div>
+            <Footer />
+
 
         </div>
-    ) : <div>Loading...</div>
+    ) : <Loader/>;
 }
 
 
