b3c40f10bd29585c4320444713f7282834b727e2
Subhraj <subhrajankit@gmail.com>
2026-01-26 15:14:27 +0530

    Initial Commit

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32 bg-primary/3'>
+        <div className='flex flex-col md:flex-row items-start justify-between ga0-10
+        py-10 border-b border-gray-500/30 text-gray-500'>
+
+        </div>
+        <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>
+            Copyright 2026 &copy; OrionBlog - All Rights Reserved.
+        </p>
+    </div>
+  )
+}
+
+export default Footer
