b3015c6dd8a392757a60ff12970f497ab22040fa
Subhraj <subhrajankit@gmail.com>
2026-01-26 22:49:17 +0530

    Login page partially done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
+import Dashboard from './pages/admin/Dashboard'
+import AddBlog from './pages/admin/AddBlog'
+import ListBlog from './pages/admin/ListBlog'
+import Comments from './pages/admin/Comments'
+import Layout from './pages/admin/Layout'
+import Login from './components/admin/Login'
 
 const App = () => {
   return (
@@ -9,6 +15,12 @@ const App = () => {
       <Routes>
         <Route path= '/' element = {<Home />} />
         <Route path= '/blog/:id' element = {<Blog />} />
+        <Route path='/admin' element={false ? <Layout /> : <Login />}>
+          <Route index element={<Dashboard />} />  
+          <Route path='addblog' element={<AddBlog />} />
+          <Route path='listblog' element={<ListBlog />} />
+          <Route path='comments' element={<Comments />} />
+        </Route>
       </Routes>
     </div>
   ) 
