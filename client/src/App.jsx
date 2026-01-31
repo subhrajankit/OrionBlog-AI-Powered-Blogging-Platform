36de56c3ab460197639bd733a56144816a98e609
Subhraj <subhrajankit@gmail.com>
2026-02-01 00:39:51 +0530

    Working on the admin Part

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
       <Routes>
         <Route path= '/' element = {<Home />} />
         <Route path= '/blog/:id' element = {<Blog />} />
-        <Route path='/admin' element={false ? <Layout /> : <Login />}>
+        <Route path='/admin' element={true ? <Layout /> : <Login />}>
           <Route index element={<Dashboard />} />  
           <Route path='addblog' element={<AddBlog />} />
           <Route path='listblog' element={<ListBlog />} />
